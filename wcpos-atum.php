3599afd0248457a1038aebdd1aac05b55ea6cd0b
Paul Kilmurray <paul@kilbot.com>
2026-04-21 01:02:50 +0200

    chore: bump version to 0.1.2

--- wcpos-atum.php
+++ wcpos-atum.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WCPOS ATUM Integration
  * Description: ATUM Multi-Inventory integration for WCPOS, linking inventory locations to POS stores.
- * Version: 0.1.1
+ * Version: 0.1.2
  * Author: kilbot
  * Requires Plugins: woocommerce, atum-stock-manager-for-woocommerce, atum-multi-inventory
  * Text Domain: wcpos-atum
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-const VERSION = '0.1.1';
+const VERSION = '0.1.2';
 
 require_once __DIR__ . '/includes/class-plugin.php';
 
