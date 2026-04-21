714a5dac18de9069505dd44621f3a5bae6c09d8e
Paul Kilmurray <paul@kilbot.com>
2026-04-21 15:26:11 +0200

    chore: add Update URI header

--- wcpos-atum.php
+++ wcpos-atum.php
@@ -4,6 +4,7 @@
  * Description: ATUM Multi-Inventory integration for WCPOS, linking inventory locations to POS stores.
  * Version: 0.1.2
  * Author: kilbot
+ * Update URI:  https://github.com/wcpos/wcpos-atum
  * Requires Plugins: woocommerce, atum-stock-manager-for-woocommerce, atum-multi-inventory
  * Text Domain: wcpos-atum
  *
